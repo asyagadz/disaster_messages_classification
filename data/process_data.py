67e97d9771053ef9dd25715579a813d1c2070197
Asya Gadzhalova <asya.gadz@gmail.com>
2022-06-16 15:51:29 +0300

    update python files

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,51 @@
 import sys
-
+import pandas as pd
+import numpy as np
+import sqlalchemy
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, how = 'inner', on = 'id')
+    return df
 
 
 def clean_data(df):
-    pass
+    #split the values in different columns
+    categories_new = df['categories'].str.split(pat=';',expand=True)
+    #change the column names 
+    categories_new.columns = categories_new.iloc[0]
+    #remove numbers from column names
+    new_list=[]
+    for col in categories_new.columns:
+    new_col = col.split('-')[0]
+    new_list.append(new_col)
+    categories_new.columns = new_list
+    #trannsform the values into binary
+    for col in categories_new.columns:
+    categories_new[col]=categories_new[col].apply(lambda x: x.split('-')[1])
+    categories_new[col]=pd.to_numeric(categories_new[col])
+    #merge back to the original data
+    df = df.join(categories_new)
+    #drop the original column 
+    df.drop(['categories'],axis=1,inplace=True)
+    #drop duplicates
+    df.drop_duplicates(subset='id',inplace=True)
+    #drop other columns
+    df.drop(columns=['original'],axis=1,inplace=True)
+    #change labels in related 
+    df.loc[df['related']==2,'related']=1
+    #drop columns with only 1 value - constant categories
+    col_list = []
+    for col in df.columns:
+        if df[col].nunique()==1:
+            col_list.append(col)
+    df.drop(col_list,axis=1,inplace=True)
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df,database_name):
+    engine = sqlalchemy.create_engine('sqlite:///{}'.format(database_name))
+    df.to_sql('table_message', engine,if_exists='replace')
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -2,23 +2,70 @@ import sys
 
 
 def load_data(database_filepath):
-    pass
+    engine = sqlalchemy.create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('table_message',con=engine)
 
 
-def tokenize(text):
-    pass
-
+def tokenize(serie):
+    '''Function to normalize data, clean special characters,clean rows with only empty strings, noise
+    '''
+    #lower case
+    serie = serie.astype(str).str.lower()
+    #cleaning
+    serie= serie.str.replace(r'://www.([\w\-\.]+\S+)','') #replace URL
+    serie= serie.str.replace(r'[^\w\s]|\b\w{1,2}\b|\d+','') #remove digit, less than 2 chars
+     #define stemmer
+    st = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    stop= stopwords.words('english') + words
+    #stop = words #I will not remove for now the stop words since they convey some meaning 
+    stop = [st.stem(x) for x in stop]
+    #define stemmer
+    st = PorterStemmer()
+    #apply tokenization & lemmatization
+    serie= serie.apply(lambda x: " ".join([lemmatizer.lemmatize(word) for word in x.split() 
+                                           if st.stem(word) not in stop])) 
+    return serie
 
 def build_model():
-    pass
+    df['tokens'] = tokenize(df['message'])
+    X = df[['tokens','genre']].copy()
+    y = df[['related', 'request', 'offer',
+       'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+       'security', 'military', 'water', 'food', 'shelter', 'clothing', 'money',
+       'missing_people', 'refugees', 'death', 'other_aid',
+       'infrastructure_related', 'transport', 'buildings', 'electricity',
+       'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+       'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+       'other_weather', 'direct_report']].copy()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10,random_state=42)   
+
+    #Pipeline 
+    preprocessor = ColumnTransformer(
+        [('genre_cat', OneHotEncoder(dtype='int', handle_unknown='ignore'), ['genre']),
+        ('description_tfidf', TfidfVectorizer( ngram_range=(1,1)))],
+        remainder='passthrough')
+
+    pipeline = Pipeline([
+        ('preprocessor',preprocessor),
+        ('classifier',MultiOutputClassifier(LinearSVC()))
+    ])     
+
+    pipeline.fit(X_train, y_train)
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    metrica = metrics.classification_report(Y_test,y_pred,target_names = y_test.columns.values)
+    metrica = pd.DataFrame(metrica).transpose()
+    for category in category_names:
+        print (metrica[metrica.index==category])
 
 
 def save_model(model, model_filepath):
-    pass
+    filename = '{}.pkl'.format(model_filepath)
+    pickle.dump(model, open(filename, 'wb'))
 
 
 def main():
