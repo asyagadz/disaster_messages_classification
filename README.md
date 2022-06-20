912161044f93bf17700ffd31329a898542affb1d
Asya Gadzhalova <asya.gadz@gmail.com>
2022-06-20 14:13:04 +0300

    update workspace folder

--- README.md
+++ README.md
@@ -1,22 +1,58 @@
 # Disaster_messages_classification
-Project under Udacity's Data Science Nanodegree 
+Project under Udacity's Data Science Nanodegree - Data Engineering part.
+Data is provided from the program. The purpose of the project is to set up a machine learning pipeline for multi-label classification of disaster messages into different categories, deploy that pipeline on a web service and create a visualisation dashboard. 
 
+## Installations 
+For reference - check the requirements.txt file in the main folder. 
 
+## Project Motivation
+Project under Udacity's Data Science Nanodegree - Data Engineering part.
+The purpose of the project is to demonstrate abilities for setting up an ETL pipeline, a ML pipeline, and set up a dashboard to communicate the results. 
 
-- app
-| - template
-| |- master.html  # main page of web app
-| |- go.html  # classification result page of web app
-|- run.py  # Flask file that runs app
+## File Descriptions
+The project contains the following folders and files: 
 
+- workspace #this folder contains all the files for the web visualisation dashboard
+|- app
+| |- template
+| | |- master.html  # main page of web app
+| | |- go.html  # classification result page of web app
+| |- run.py  # Flask file that runs app
+
+|- data
+| |- disaster_categories.csv  # data to process 
+| |- disaster_messages.csv  # data to process
+| |- process_data.py        # file to clean and transform the initial data 
+| |- ResponseDisaster.db   # database to save clean data to
+
+| - models
+| |- train_classifier.py # file to build the machine learning pipeline
+| |- classifier.pkl  # saved model 
+
+- notebooks 
+| - 01.01.-ag-initial-analysis-cleaning.ipynb # data cleaning, transformation and EDA of the initial data
+| - 02.01.-ag-data-cleaning.ipynb # the notebook deals with the text processing and text preparation to be transformed into numerical data
+  - 02.02.-ag-data-cleaning-text-preprocessing-spacy.ipynb  # it is a complementary notebook that used spacy library and its nlp pipeline for all text processing tasks; 
+  - 03.01.-ag-model-pipeline.ipynb # this notebook contains the ML pipeline
+  
 - data
-|- disaster_categories.csv  # data to process 
-|- disaster_messages.csv  # data to process
-|- process_data.py
-|- InsertDatabaseName.db   # database to save clean data to
-
-- models
-|- train_classifier.py
-|- classifier.pkl  # saved model 
+| - raw data # the raw files
+| | - disaster_messages.csv
+| | - disaster_categories.csv
+| - processed data # the files processed throughtout the different steps of the project
+| | - data_after_eda.pkl # the file with the cleaned data, output from 01.01. notebook
+| | - data_after_prcessing_spacy.pkl # output from notebook 02.02.; suplementary file
+| | - data_after_text_processing.pkl # output from notebook 02.01.; file with text preprossed data
+| - preprocess_help_functions.py #file that contains some additional functions and transformations tested on the data - e.g. translation, language detection etc.
 
 - README.md
+
+## How to Interact with your project/Results
+TBased on the analysis performed, we have seen some interesting information about the AirBNBs in Boston:
+
+- beginning of Autumn is the busiest period for the airBNBs occupancy rates
+- there are neighbourhoods that reveal a capacity for new AirBNBs.
+- a more precise modelling and data mining is necessary to build more robust model, that explains better what affect the prices and its values.
+
+## Licensing, Authors, Acknowledgements, etc.
+I have used some of the code from the lectures from the python files.
---
+++ workspace/models/train_classifier.py
@@ -0,0 +1,113 @@
+import sys
+import pandas as pd
+import numpy as np 
+import sqlalchemy
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score, f1_score, average_precision_score
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import pickle
+
+def load_data(database_filepath):
+    engine = sqlalchemy.create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('table_message',con=engine)
+    X = df['message']
+    y = df[['related', 'request', 'offer',
+       'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+       'security', 'military', 'water', 'food', 'shelter', 'clothing', 'money',
+       'missing_people', 'refugees', 'death', 'other_aid',
+       'infrastructure_related', 'transport', 'buildings', 'electricity',
+       'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+       'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+       'other_weather', 'direct_report']]
+    category_names = y.columns
+    return X, y, category_names 
+
+def tokenize(text):
+    #cleaning
+    text= text.replace(r'://www.([\w\-\.]+\S+)','') #replace URL
+    text= text.replace(r'[^\w\s]|\b\w{1,2}\b|\d+','') #remove digit, less than 2 chars
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    st = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    stop= stopwords.words('english')
+    #stop = words #I will not remove for now the stop words since they convey some meaning 
+    stop = [st.stem(x) for x in stop]
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip() 
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens    
+ 
+
+def build_model():
+    #Pipeline 
+    model = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('classifier',MultiOutputClassifier(LinearSVC()))
+    ])     
+
+    return model
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    print (metrics.classification_report(Y_test,y_pred,target_names = category_names))
+
+
+def save_model(model, model_filepath):
+    filename = '{}'.format(model_filepath)
+    pickle.dump(model, open(filename, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
